a4ec40cefa5998b10c4d730412f5e466f702afe3
reetikraj <reetik.raj.chy17@iitbhu.ac.in>
2020-08-05 12:33:29 +0530

    Live to dominate

---
+++ src/app/api/omdb.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class OmdbService {
+  movieTitle: string;
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  searchMovies(): Observable<any> {
+    if (this.movieTitle === undefined || this.movieTitle === null) { this.movieTitle = ''; }
+    const url = 'https://omdb-backend.herokuapp.com/api/omdb/search?title=' + this.movieTitle;
+    
+    return this.http.get(url);
+  }
+}
